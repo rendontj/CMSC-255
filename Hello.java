0c6a9cb95ce36e1f515515062bc6cc59ba181182
rendontj <81486447+rendontj@users.noreply.github.com>
2021-05-24 16:26:54 -0400

    Add files via upload
    Here is my first laboratory assignment, lab1.

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/************************************
+ * Lab 01
+ ************************************
+ * Taylor Rendon 
+ * 5.24.21
+ * CMSMC.255.C01
+ ***********************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+	
+	System.out.println("Taylor Rendon");
+	System.out.println("Lab 01");
+	System.out.println("This is our first laboratory assignment. The goal of this lab is to create and run a Java program.");
+	System.out.println("5.24.21");
+	System.out.println("CMSMC.255.C01");
+	System.out.println();
+	}
+}
